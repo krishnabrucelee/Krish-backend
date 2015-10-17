5130586a9c4be4ddbdd33ddc9f26de2f4831e1b4
Mohanapriya Rathinasamy <mohanapriya@assistanz.com>
2015-10-17 13:33:32 +0530

    Added CRUD API support for cloudstack configuration

---
+++ src/main/java/ck/panda/domain/repository/jpa/CloudStackConfigurationRepository.java
@@ -0,0 +1,14 @@
+package ck.panda.domain.repository.jpa;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import ck.panda.domain.entity.CloudStackConfiguration;
+
+/**
+ * JPA repository for CloudStackConfiguration entity.
+ */
+public interface CloudStackConfigurationRepository extends PagingAndSortingRepository<CloudStackConfiguration, Long> {
+
+	// TODO for validation
+//	@Query(value = "select config from CloudStackConfiguration config where config.apiKey=:apiKey ")
+//	CloudStackConfiguration findByKeys(@Param("apiKey") String apiKey);
+}
---
+++ src/main/java/ck/panda/service/CloudStackConfigurationService.java
@@ -0,0 +1,16 @@
+package ck.panda.service;
+
+import org.springframework.stereotype.Service;
+import ck.panda.domain.entity.CloudStackConfiguration;
+import ck.panda.util.domain.CRUDService;
+
+/**
+ * Service interface for the CloudStack configuration entity.
+ */
+@Service
+public interface CloudStackConfigurationService  extends CRUDService<CloudStackConfiguration> {
+
+	// TODO for validation.
+	//CloudStackConfiguration findByKeys(String apiKey);
+
+	}
