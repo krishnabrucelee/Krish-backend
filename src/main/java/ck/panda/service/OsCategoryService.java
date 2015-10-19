cd153152e5f24bb383810ea578e71dab0a34ba43
prakash <prakash.n@assistanz.com>
2015-10-19 16:54:19 +0530

    Added OS category sync service for template creation

---
+++ src/main/java/ck/panda/service/OsCategoryService.java
@@ -0,0 +1,14 @@
+package ck.panda.service;
+
+import org.springframework.stereotype.Service;
+import ck.panda.domain.entity.OsCategory;
+import ck.panda.util.domain.CRUDService;
+
+/**
+ * Service interface for OS category entity.
+ *
+ */
+@Service
+public interface OsCategoryService extends CRUDService<OsCategory> {
+
+}
---
+++ src/main/java/ck/panda/service/OsCategoryServiceImpl.java
@@ -0,0 +1,59 @@
+package ck.panda.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import ck.panda.domain.entity.OsCategory;
+import ck.panda.domain.repository.jpa.OsCategoryRepository;
+import ck.panda.util.domain.vo.PagingAndSorting;
+
+/**
+ * Service implementation for OS category entity
+ *
+ */
+@Service
+public class OsCategoryServiceImpl implements OsCategoryService {
+
+	/** OS category repository reference. */
+	@Autowired
+	private OsCategoryRepository oscategoryrepository;
+
+	@Override
+	public List<OsCategory> findAll() throws Exception {
+		return (List<OsCategory>) oscategoryrepository.findAll();
+	}
+
+	@Override
+	public OsCategory save(OsCategory oscategory) throws Exception {
+		return oscategoryrepository.save(oscategory);
+	}
+
+	@Override
+	public OsCategory update(OsCategory oscategory) throws Exception {
+		return oscategoryrepository.save(oscategory);
+	}
+
+	/** No need of implementation. */
+	@Override
+	public void delete(OsCategory oscategory) throws Exception {
+	}
+
+	/** No need of implementation. */
+	@Override
+	public void delete(Long id) throws Exception {
+	}
+
+	/** No need of implementation. */
+	@Override
+	public OsCategory find(Long id) throws Exception {
+		return null;
+	}
+
+	/** No need of implementation. */
+	@Override
+	public Page<OsCategory> findAll(PagingAndSorting pagingAndSorting) throws Exception {
+		return null;
+	}
+
+}
