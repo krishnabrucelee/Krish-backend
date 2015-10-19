fdc00a2a8bb5bc858fc1351006bc65c90676d2e9
Assistanz <Assistanz@localhost.localdomain>
2015-10-19 20:27:29 +0530

    User Registration added.have to do validation,edit,delete

---
+++ src/main/java/ck/panda/domain/entity/UserType.java
@@ -0,0 +1,12 @@
+package ck.panda.domain.entity;
+
+/**
+ * Define user type.
+ *
+ */
+public enum UserType {
+    /**
+     * Define user type constant.
+     */
+    USER,ADMIN;
+}
--- src/main/java/ck/panda/domain/repository/jpa/UserRepository.java
+++ src/main/java/ck/panda/domain/repository/jpa/UserRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import ck.panda.domain.entity.User;
 
 /**
- *
- * JPA repository for user CRUD operations.
+ * 
+ * JPA repository for user CRUD operations
  */
 
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
