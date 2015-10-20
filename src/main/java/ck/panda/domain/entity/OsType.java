3a92522cfeb5b9865bebfef18b714924d3e47f65
prakash <prakash.n@assistanz.com>
2015-10-20 17:55:07 +0530

    Added OS type sync service for template creation

--- src/main/java/ck/panda/domain/entity/OsType.java
+++ src/main/java/ck/panda/domain/entity/OsType.java
@@ -1,10 +1,6 @@
 package ck.panda.domain.entity;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,40 +9,43 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Version;
-import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.joda.time.DateTime;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 /**
- * OS category purpose is to create which category of operation system you want when creating the template.
- * Get the OS category list from cloud stack server and push into the application database
- * When creating the template and instance fetch the OS category from application database
+ * OS type purpose is to create which type of operation system you want when creating the template.
+ * Get the OS type list from cloud stack server and push into the application database
+ * When creating the template fetch the OS type from application database
  *
  */
 @Entity
-@Table(name = "ck_os_category")
+@Table(name = "ck_os_type")
 @SuppressWarnings("serial")
-public class OsCategory implements Serializable {
+public class OsType implements Serializable {
 
-    /** Id of the OS category. */
+    /** Id of the OS type. */
     @Id
     @GeneratedValue
     @Column(name = "id")
     private Long id;
 
-    /** Name of the OS category. */
-    @Size(min = 4, max = 25)
-    @Column(name = "name", nullable = false)
-    private String name;
-
-    /** Unique id of the OS category. */
+    /** Unique id of the OS type. */
     @Column(name = "uuid")
     private String uuid;
+
+    /** OS category type id. */
+    @NotEmpty
+    @JoinColumn(name = "oscategory_id", referencedColumnName = "id")
+    @OneToOne
+    private OsCategory osCategoryId;
+
+    /** Display name of the OS type. */
+    @Column(name = "display_name")
+    private String displayName;
 
     /** Version attribute to handle optimistic locking. */
     @Version
@@ -92,22 +91,6 @@ public class OsCategory implements Serializable {
     }
 
     /**
-     * Get the name.
-     * @return name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * Set the name.
-     * @param name - the String to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
      * Get the UUID.
      * @return uuid
      */
@@ -121,6 +104,38 @@ public class OsCategory implements Serializable {
      */
     public void setUuid(String uuid) {
         this.uuid = uuid;
+    }
+
+    /**
+     * Get the OS category id.
+     * @return osCategoryId
+     */
+    public OsCategory getOsCategoryId() {
+        return osCategoryId;
+    }
+
+    /**
+     * Set the OS category id.
+     * @param osCategoryId - the OsCategory entity to set
+     */
+    public void setOsCategoryId(OsCategory osCategoryId) {
+        this.osCategoryId = osCategoryId;
+    }
+
+    /**
+     * Get the display name.
+     * @return displayName
+     */
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    /**
+     * Set the display name.
+     * @param displayName - the String to set
+     */
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
     }
 
     /**
@@ -201,35 +216,5 @@ public class OsCategory implements Serializable {
      */
     public void setUpdatedDateTime(DateTime updatedDateTime) {
         this.updatedDateTime = updatedDateTime;
-    }
-
-    /**
-     * Convert JSONObject to oscategory entity.
-     *
-     * @param object json object.
-     * @return oscategory entity object
-     * @throws JSONException unhandled json errors.
-     */
-    public static OsCategory convert(JSONObject object) throws JSONException {
-        OsCategory osCategory = new OsCategory();
-        osCategory.uuid = object.get("id").toString();
-        osCategory.name = object.get("name").toString();
-
-        return osCategory;
-    }
-
-    /**
-     * Mapping the os categories entity in list.
-     *
-     * @param  osCategoryList list of operating systems
-     * @return mapped values
-     */
-    public static Map<String, OsCategory> convert(List<OsCategory> osCategoryList) {
-        Map<String, OsCategory> osCategoryMap = new HashMap<String, OsCategory>();
-
-        for (OsCategory osCategory : osCategoryList) {
-             osCategoryMap.put(osCategory.getUuid(), osCategory);
-        }
-        return  osCategoryMap;
     }
 }
