46fddb3b1b211f08fc75aeb57cebdb5bea64496e
Ibrahim <ibrahim@assistanz.com>
2015-10-20 21:17:47 +0530

    Java Doc and spaces fixed.

--- src/main/java/ck/panda/service/ProjectServiceImpl.java
+++ src/main/java/ck/panda/service/ProjectServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
-
 import ck.panda.domain.entity.Project;
 import ck.panda.domain.repository.jpa.ProjectRepository;
 import ck.panda.util.domain.vo.PagingAndSorting;
