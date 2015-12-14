1aa92d4c1c5846a834785bcd7ee674553ad70524
prakash <prakash.n@assistanz.com>
2015-12-14 10:49:05 +0530

    Updated the upstream data

--- src/main/java/ck/panda/web/resource/LogoutController.java
+++ src/main/java/ck/panda/web/resource/LogoutController.java
@@ -6,29 +6,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import com.wordnik.swagger.annotations.Api;
-import ck.panda.util.web.ApiController;
 
 /**Template controller. */
 @RestController
 @RequestMapping("/api/logout")
-@Api(value = "Logout", description = "Logout current user", produces = "application/json")
-public class LogoutController implements ApiController {
+public class LogoutController {
 	@RequestMapping(method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
 	@ResponseStatus(HttpStatus.OK)
 	@ResponseBody
-	public Boolean read(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	public String read(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if (auth != null) {
-			auth.setAuthenticated(false);
-			SecurityContextHolder.clearContext();
-			return true;
-		}
-		return false;
+	    if (auth != null){
+	        new SecurityContextLogoutHandler().logout(request, response, auth);
+	    }
+	    return "redirect:/login?logout";
 	}
 }
