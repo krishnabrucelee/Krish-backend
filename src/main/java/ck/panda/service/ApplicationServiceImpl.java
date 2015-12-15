fd57c43296ff47fea1ecb2cb724574a515be4d82
Mohammad Yashin IqbalDeen <mohammadyashin@assistanz.com>
2015-12-15 11:03:45 +0530

    Fixed the issues in application module

--- src/main/java/ck/panda/service/ApplicationServiceImpl.java
+++ src/main/java/ck/panda/service/ApplicationServiceImpl.java
@@ -1,11 +1,11 @@
 package ck.panda.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import ck.panda.domain.entity.Application;
+import ck.panda.domain.entity.Application.Status;
 import ck.panda.domain.entity.Domain;
 import ck.panda.domain.repository.jpa.ApplicationRepository;
 import ck.panda.domain.repository.jpa.DomainRepository;
@@ -28,9 +28,9 @@ public class ApplicationServiceImpl implements ApplicationService {
     @Autowired
     private ApplicationRepository applicationRepo;
 
-    /** Autowired TokenDetails */
+    /** Autowired TokenDetails. */
     @Autowired
-    TokenDetails tokenDetails;
+    private TokenDetails tokenDetails;
 
     /** Domain repository reference. */
     @Autowired
@@ -53,8 +53,8 @@ public class ApplicationServiceImpl implements ApplicationService {
     private void validateApplication(Application application) throws Exception {
         Errors errors = validator.rejectIfNullEntity("application", application);
         errors = validator.validateEntity(application, errors);
-        Application app = applicationRepo.findByTypeAndDomainAndIsActive(application.getType(), application.getDomain(),
-                true);
+        Application app = applicationRepo.findByTypeAndDomainAndIsActive(application.getType(), application.getDomain(), true,
+                Status.ENABLED);
         if (app != null && application.getId() != app.getId()) {
             errors.addFieldError("type", "application.already.exist.for.same.domain");
         }
@@ -91,12 +91,12 @@ public class ApplicationServiceImpl implements ApplicationService {
     @Override
     public Page<Application> findAll(PagingAndSorting pagingAndSorting) throws Exception {
         Domain domain = domainRepository.findOne(Long.valueOf(tokenDetails.getTokenDetails("domainid")));
-        if(domain != null && !domain.getName().equals("ROOT")) {
-			if (Long.valueOf(tokenDetails.getTokenDetails("departmentid")) == 1000L) {
-				return applicationRepo.findAllByIsActive(pagingAndSorting.toPageRequest(), true);
-			} else {
-				return applicationRepo.findAllByDomainIsActive(domain.getId(), true, pagingAndSorting.toPageRequest());
-			}
+        if (domain != null && !domain.getName().equals("ROOT")) {
+            if (Long.valueOf(tokenDetails.getTokenDetails("departmentid")) == 1000L) {
+                return applicationRepo.findAllByIsActive(pagingAndSorting.toPageRequest(), true);
+            } else {
+                return applicationRepo.findAllByDomainIsActive(domain.getId(), true, pagingAndSorting.toPageRequest());
+            }
         }
         return applicationRepo.findAllByIsActive(pagingAndSorting.toPageRequest(), true);
     }
@@ -104,12 +104,12 @@ public class ApplicationServiceImpl implements ApplicationService {
     @Override
     public List<Application> findAll() throws Exception {
         Domain domain = domainRepository.findOne(Long.valueOf(tokenDetails.getTokenDetails("domainid")));
-        if(domain != null && !domain.getName().equals("ROOT")) {
-			if (Long.valueOf(tokenDetails.getTokenDetails("departmentid")) == 1000L) {
-				return (List<Application>) applicationRepo.findAllByIsActive(true);
-			} else {
-				return applicationRepo.findAllByDomain(domain.getId(), true);
-			}
+        if (domain != null && !domain.getName().equals("ROOT")) {
+            if (Long.valueOf(tokenDetails.getTokenDetails("departmentid")) == 1000L) {
+                return (List<Application>) applicationRepo.findAllByIsActive(true);
+            } else {
+                return applicationRepo.findAllByDomainIsActive(domain.getId(), true);
+            }
         }
         return (List<Application>) applicationRepo.findAllByIsActive(true);
     }
@@ -117,26 +117,26 @@ public class ApplicationServiceImpl implements ApplicationService {
     @Override
     public Page<Application> findAllByActive(PagingAndSorting pagingAndSorting) throws Exception {
         Domain domain = domainRepository.findOne(Long.valueOf(tokenDetails.getTokenDetails("domainid")));
-        if(domain != null && !domain.getName().equals("ROOT")) {
-            return applicationRepo.findAllByDomain(domain.getId(), pagingAndSorting.toPageRequest());
+        if (domain != null && !domain.getName().equals("ROOT")) {
+            return applicationRepo.findAllByDomainIsActiveAndStatus(domain.getId(), pagingAndSorting.toPageRequest(), true, Status.ENABLED);
         }
-        return applicationRepo.findAllByIsActive(pagingAndSorting.toPageRequest(), true);
+        return applicationRepo.findAllByIsActiveAndStatus(pagingAndSorting.toPageRequest(), true, Status.ENABLED);
     }
 
     @Override
     public Application softDelete(Application application) throws Exception {
         application.setIsActive(false);
-        application.setStatus(Application.Status.DISABLED);
+        application.setStatus(Status.DISABLED);
         return applicationRepo.save(application);
     }
 
     @Override
     public List<Application> findAllByIsActive(Boolean isActive) throws Exception {
-    	Domain domain = domainRepository.findOne(Long.valueOf(tokenDetails.getTokenDetails("domainid")));
-        if(domain != null && !domain.getName().equals("ROOT")) {
-            return applicationRepo.findAllByIsActiveAndDomain(domain.getId(), true);
+        Domain domain = domainRepository.findOne(Long.valueOf(tokenDetails.getTokenDetails("domainid")));
+        if (domain != null && !domain.getName().equals("ROOT")) {
+            return applicationRepo.findAllByIsActiveAndDomainAndStatus(domain.getId(), true, Status.ENABLED);
         }
-    	return applicationRepo.findAllByIsActive(true);
+        return applicationRepo.findAllByIsActiveAndStatus(true, Status.ENABLED);
     }
 
 }
