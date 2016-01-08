78007400bd5662dc833bc1a2c483e285217dd1c3
Krishna <krishna@assistanz.com>
2016-01-08 19:21:37 +0530

    Port forwarding dummy methods done. Waiting for approval to proceed forward.

---
+++ src/main/java/ck/panda/domain/entity/PortForwarding.java
@@ -0,0 +1,283 @@
+/**
+ *
+ */
+package ck.panda.domain.entity;
+
+import java.time.ZonedDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import org.hibernate.annotations.Type;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+/**
+ * Port forwarding.
+ *
+ */
+@Entity
+@Table(name = "port_forwarding_rule")
+public class PortForwarding {
+
+    /** Unique Id of the Port Forwarding. */
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    /** Cloudstack's Port Forwarding uuid. */
+    @Column(name = "uuid")
+    private String uuid;
+
+    /** ipAddress of the Port Forwarding. */
+    @JoinColumn(name = "ipaddress_id", referencedColumnName = "Id", updatable = false, insertable = false)
+    @ManyToOne
+    private IpAddress ipAddress;
+
+    /** ipAddress id of the Port Forwarding. */
+    @Column(name = "ipaddress_id")
+    private Long ipAddressId;
+
+    /** Starting port of the firewall rule. */
+    @Column(name = "dest_port_start")
+    private Integer destPortStart;
+
+    /** Ending port of the firewall rule. */
+    @Column(name = "dest_port_end")
+    private Integer destPortEnd;
+
+    /** An active attribute is to check whether the role is active or not. */
+    @Column(name = "is_active", columnDefinition = "tinyint default 1")
+    @Type(type = "org.hibernate.type.NumericBooleanType")
+    private Boolean isActive;
+
+    /** Created by user. */
+    @CreatedBy
+    @Column(name = "created_user_id")
+    private Long createdBy;
+
+    /** Last updated by user. */
+    @LastModifiedBy
+    @Column(name = "upated_user_id")
+    private Long updatedBy;
+
+    /** Created date and time. */
+    @CreatedDate
+    @Column(name = "created_date_time")
+    @Type(type = "org.jadira.usertype.dateandtime.threeten.PersistentZonedDateTime")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private ZonedDateTime createdDateTime;
+
+    /** Last modified date and time. */
+    @LastModifiedDate
+    @Column(name = "updated_date_time")
+    @Type(type = "org.jadira.usertype.dateandtime.threeten.PersistentZonedDateTime")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private ZonedDateTime updatedDateTime;
+
+    /**
+     * Get the id of the PortForwarding.
+
+     * @return the id of PortForwarding.
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Set the id of the PortForwarding.
+     *
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * Get the uuid of the PortForwarding.
+
+     * @return the uuid of PortForwarding.
+     */
+    public String getUuid() {
+        return uuid;
+    }
+
+    /**
+     * Set the uuid of the PortForwarding.
+     *
+     * @param uuid the uuid to set
+     */
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    /**
+     * Get the ipAddress of the PortForwarding.
+
+     * @return the ipAddress of PortForwarding.
+     */
+    public IpAddress getIpAddress() {
+        return ipAddress;
+    }
+
+    /**
+     * Set the ipAddress of the PortForwarding.
+     *
+     * @param ipAddress the ipAddress to set
+     */
+    public void setIpAddress(IpAddress ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    /**
+     * Get the ipAddressId of the PortForwarding.
+
+     * @return the ipAddressId of PortForwarding.
+     */
+    public Long getIpAddressId() {
+        return ipAddressId;
+    }
+
+    /**
+     * Set the ipAddressId of the PortForwarding.
+     *
+     * @param ipAddressId the ipAddressId to set
+     */
+    public void setIpAddressId(Long ipAddressId) {
+        this.ipAddressId = ipAddressId;
+    }
+
+    /**
+     * Get the destPortStart of the PortForwarding.
+
+     * @return the destination port start of PortForwarding.
+     */
+    public Integer getDestPortStart() {
+        return destPortStart;
+    }
+
+    /**
+     * Set the destPortStart of the PortForwarding.
+     *
+     * @param destPortstart the destination port start to set
+     */
+    public void setDestPortStart(Integer destPortStart) {
+        this.destPortStart = destPortStart;
+    }
+
+    /**
+     * Get the destPortEnd of the PortForwarding.
+
+     * @return the destination port end of PortForwarding.
+     */
+    public Integer getDestPortEnd() {
+        return destPortEnd;
+    }
+
+    /**
+     * Set the destPortEnd of the PortForwarding.
+     *
+     * @param destPortEnd the destination port end to set
+     */
+    public void setDestPortEnd(Integer destPortEnd) {
+        this.destPortEnd = destPortEnd;
+    }
+
+    /**
+     * Get the isActive of the PortForwarding.
+
+     * @return the isActive of PortForwarding.
+     */
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    /**
+     * Set the isActive of the PortForwarding.
+     *
+     * @param isActive the isActive to set
+     */
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    /**
+     * Get the createdBy of the PortForwarding.
+
+     * @return the createdBy of PortForwarding.
+     */
+    public Long getCreatedBy() {
+        return createdBy;
+    }
+
+    /**
+     * Set the createdBy of the PortForwarding.
+     *
+     * @param createdBy the createdBy to set
+     */
+    public void setCreatedBy(Long createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    /**
+     * Get the updatedBy of the PortForwarding.
+
+     * @return the updatedBy of PortForwarding.
+     */
+    public Long getUpdatedBy() {
+        return updatedBy;
+    }
+
+    /**
+     * Set the updatedBy of the PortForwarding.
+     *
+     * @param updatedBy the updatedBy to set
+     */
+    public void setUpdatedBy(Long updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    /**
+     * Get the createdDateTime of the PortForwarding.
+
+     * @return the createdDateTime of PortForwarding.
+     */
+    public ZonedDateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    /**
+     * Set the createdDateTime of the PortForwarding.
+     *
+     * @param createdDateTime the createdDateTime to set
+     */
+    public void setCreatedDateTime(ZonedDateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+    /**
+     * Get the updatedDateTime of the PortForwarding.
+
+     * @return the updatedDateTime of PortForwarding.
+     */
+    public ZonedDateTime getUpdatedDateTime() {
+        return updatedDateTime;
+    }
+
+    /**
+     * Set the updatedDateTime of the PortForwarding.
+     *
+     * @param updatedDateTime the updatedDateTime to set
+     */
+    public void setUpdatedDateTime(ZonedDateTime updatedDateTime) {
+        this.updatedDateTime = updatedDateTime;
+    }
+
+}
---
+++ src/main/java/ck/panda/web/resource/PortForwardingController.java
@@ -0,0 +1,67 @@
+/**
+ *
+ */
+package ck.panda.web.resource;
+
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import ck.panda.constants.GenericConstants;
+import ck.panda.domain.entity.FirewallRules;
+import ck.panda.domain.entity.PortForwarding;
+import ck.panda.service.PortForwardingService;
+import ck.panda.util.domain.vo.PagingAndSorting;
+import ck.panda.util.web.ApiController;
+import ck.panda.util.web.CRUDController;
+
+/**
+ * Port Forwarding Controller.
+ *
+ */
+@RestController
+@RequestMapping("/api/portforwarding")
+@Api(value = "PortForwarding", description = "Operations with PortForwarding", produces = "application/json")
+public class PortForwardingController extends CRUDController<PortForwarding> implements ApiController {
+
+    /** Service reference to Port Forwarding. */
+    @Autowired
+    private PortForwardingService portForwardingService;
+
+    @ApiOperation(value = SW_METHOD_CREATE, notes = "Create a new Port Forwarding.", response = PortForwarding.class)
+    @Override
+    public PortForwarding create(@RequestBody PortForwarding portForwarding) throws Exception {
+        return portForwardingService.save(portForwarding);
+    }
+
+    @ApiOperation(value = SW_METHOD_READ, notes = "Read an existing Port Forwarding.", response = PortForwarding.class)
+    @Override
+    public PortForwarding read(@PathVariable(PATH_ID) Long id) throws Exception {
+        return portForwardingService.find(id);
+    }
+
+    @ApiOperation(value = SW_METHOD_UPDATE, notes = "Update an existing port Forwarding.", response = PortForwarding.class)
+    @Override
+    public PortForwarding update(@RequestBody PortForwarding portForwarding, @PathVariable(PATH_ID) Long id)
+            throws Exception {
+        return portForwardingService.update(portForwarding);
+    }
+
+    @Override
+    public List<PortForwarding> list(@RequestParam String sortBy, @RequestHeader(value = RANGE) String range,
+            @RequestParam Integer limit, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        PagingAndSorting page = new PagingAndSorting(range, sortBy, limit, FirewallRules.class);
+        Page<PortForwarding> pageResponse = portForwardingService.findAll(page);
+        response.setHeader(GenericConstants.CONTENT_RANGE_HEADER, page.getPageHeaderValue(pageResponse));
+        return pageResponse.getContent();
+    }
+}
