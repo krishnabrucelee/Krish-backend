60aea4f4e410c354650d7fe77d380adaf807e30c
prakash <prakash.n@assistanz.com>
2016-01-14 21:36:28 +0530

    Updated the loadebalancer functionality

--- src/main/java/ck/panda/web/resource/LoadBalancerController.java
+++ src/main/java/ck/panda/web/resource/LoadBalancerController.java
@@ -5,17 +5,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import ck.panda.constants.GenericConstants;
 import ck.panda.domain.entity.FirewallRules;
 import ck.panda.domain.entity.LoadBalancerRule;
+import ck.panda.domain.entity.Volume;
 import ck.panda.service.LoadBalancerService;
 import ck.panda.util.domain.vo.PagingAndSorting;
 import ck.panda.util.web.ApiController;
@@ -56,6 +62,8 @@ public class LoadBalancerController extends CRUDController<LoadBalancerRule> imp
     }
 
     @ApiOperation(value = SW_METHOD_DELETE, notes = "Delete an existing Load Balancer.")
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE, produces = {
+            MediaType.APPLICATION_JSON_VALUE })
     @Override
     public void delete(@PathVariable(PATH_ID) Long id) throws Exception {
         LoadBalancerRule loadBalancer = loadBalancerService.find(id);
@@ -70,6 +78,19 @@ public class LoadBalancerController extends CRUDController<LoadBalancerRule> imp
         Page<LoadBalancerRule> pageResponse = loadBalancerService.findAll(page);
         response.setHeader(GenericConstants.CONTENT_RANGE_HEADER, page.getPageHeaderValue(pageResponse));
         return pageResponse.getContent();
+    }
+
+   /**
+    * list all Volumes for instance.
+    *
+    * @return Volume service
+     * @throws Exception error
+     */
+    @RequestMapping(value = "list", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
+    @ResponseStatus(HttpStatus.OK)
+   @ResponseBody
+    protected List<LoadBalancerRule> getSearch(@RequestParam("ipAddressId") Long ipAddressId) throws Exception {
+        return loadBalancerService.findByIpaddress(ipAddressId, true);
     }
 
 }
