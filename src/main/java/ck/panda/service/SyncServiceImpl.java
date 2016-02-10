81804156a64081a450ec8edeb1748fb09ac5cff9
Ibrahim <ibrahim@assistanz.com>
2016-02-10 20:55:37 +0530

    project sync while edit display name

--- src/main/java/ck/panda/service/SyncServiceImpl.java
+++ src/main/java/ck/panda/service/SyncServiceImpl.java
@@ -1853,15 +1853,13 @@ public class SyncServiceImpl implements SyncService {
             // finding it in a hash using uuid
             if (csProjectMap.containsKey(project.getUuid())) {
                 Project csProject = csProjectMap.get(project.getUuid());
-
                 project.setName(csProject.getName());
                 project.setDepartmentId(csProject.getDepartmentId());
                 project.setStatus(csProject.getStatus());
+                project.setName(csProject.getDescription());
                 project.setDomainId(csProject.getDomainId());
-
                 // 3.2 If found, update the project object in app db
                 projectService.update(project);
-
                 // 3.3 Remove once updated, so that we can have the list of cs
                 // project which is not added in the app
                 csProjectMap.remove(project.getUuid());
