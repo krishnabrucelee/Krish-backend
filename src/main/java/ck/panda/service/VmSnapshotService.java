029acbd05d2eb6ce112872c022aeb31050a73acb
prakash <prakash.n@assistanz.com>
2016-03-10 19:13:35 +0530

    Fixed the check style issue

--- src/main/java/ck/panda/service/VmSnapshotService.java
+++ src/main/java/ck/panda/service/VmSnapshotService.java
@@ -1,7 +1,6 @@
 package ck.panda.service;
 
 import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import ck.panda.domain.entity.VmSnapshot;
