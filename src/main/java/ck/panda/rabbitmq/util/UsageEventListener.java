da7e155b112b92922db2a038134a34c30f640064
Ibrahim <ibrahim@assistanz.com>
2016-03-31 19:20:37 +0530

    Websocket issue fixed

--- src/main/java/ck/panda/rabbitmq/util/UsageEventListener.java
+++ src/main/java/ck/panda/rabbitmq/util/UsageEventListener.java
@@ -10,5 +10,6 @@ import org.springframework.amqp.core.MessageListener;
 public class UsageEventListener implements MessageListener {
     @Override
     public void onMessage(Message message) {
+    	System.out.println(new String(message.getBody()));
     }
 }
