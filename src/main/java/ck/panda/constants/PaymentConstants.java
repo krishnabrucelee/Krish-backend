5a09fe47d8a321c6df65e700619f3ddcf434e9f1
Ibrahim <ibrahim@assistanz.com>
2016-04-07 12:01:36 +0530

    payment gateway integration for alipay

---
+++ src/main/java/ck/panda/constants/PaymentConstants.java
@@ -0,0 +1,24 @@
+package ck.panda.constants;
+
+
+/**
+ * The following code is sample code to test for the convenience of business and provided businesses as needed for your site, according to technical writing , not have to use the code.
+ * The code for study and research Alipay interface , only provide a reference .
+ */
+public class PaymentConstants {
+
+	// Signature scheme.
+	public static String SIGN_TYPE = "MD5";
+
+	// The character encoding format is currently supported gbk or utf-8.
+	public static String INPUT_CHARSET = "utf-8";
+
+	// Payment type , without modification.
+	public static String PAYMENT_TYPE = "1";
+
+	// Anti-phishing timestamp To use the class file , call the submit function query_timestamp.
+	public static String ANTI_PHISHING_KEY = "";
+
+	// IP address of the client 's non- LAN WAN IP address, if any such as : 221.0.0.1
+	public static String EXTER_INVOKE_IP = "";
+}
---
+++ src/main/java/ck/panda/payment/util/HttpResponse.java
@@ -0,0 +1,66 @@
+package ck.panda.payment.util;
+
+import org.apache.commons.httpclient.Header;
+import ck.panda.constants.PaymentConstants;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * The following code is sample code to test for the convenience of business and
+ * provided businesses as needed for your site, according to technical writing ,
+ * not have to use the code. The code for study and research Alipay interface ,
+ * only provide a reference.
+ */
+public class HttpResponse {
+
+	/**
+	 * Back in the Header information
+	 */
+	private Header[] responseHeaders;
+
+	/**
+	 * String type result.
+	 */
+	private String stringResult;
+
+	/**
+	 * byte type of result
+	 */
+	private byte[] byteResult;
+
+	public Header[] getResponseHeaders() {
+		return responseHeaders;
+	}
+
+	public void setResponseHeaders(Header[] responseHeaders) {
+		this.responseHeaders = responseHeaders;
+	}
+
+	public byte[] getByteResult() {
+		if (byteResult != null) {
+			return byteResult;
+		}
+		if (stringResult != null) {
+			return stringResult.getBytes();
+		}
+		return null;
+	}
+
+	public void setByteResult(byte[] byteResult) {
+		this.byteResult = byteResult;
+	}
+
+	public String getStringResult() throws UnsupportedEncodingException {
+		if (stringResult != null) {
+			return stringResult;
+		}
+		if (byteResult != null) {
+			return new String(byteResult, PaymentConstants.INPUT_CHARSET);
+		}
+		return null;
+	}
+
+	public void setStringResult(String stringResult) {
+		this.stringResult = stringResult;
+	}
+
+}
