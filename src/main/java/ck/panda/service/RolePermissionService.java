1760cf091093a6eb9159dca6cfb7d5f5e5a0a394
Jamseer N <jamseer@assistanz.com>
2016-04-12 19:13:22 +0530

    Removed the unwanted spaces from the import files

--- src/main/java/ck/panda/service/RolePermissionService.java
+++ src/main/java/ck/panda/service/RolePermissionService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
-
 import ck.panda.domain.entity.Permission;
 import ck.panda.domain.entity.Role;
 import ck.panda.domain.entity.User;
