2bf537ed402031d140904615ce8f0983319d49e2
Krishna <krishna@assistanz.com>
2016-05-23 17:17:09 +0530

    Comment for controller added.

--- src/main/java/ck/panda/web/resource/HomeController.java
+++ src/main/java/ck/panda/web/resource/HomeController.java
@@ -3,10 +3,10 @@ package ck.panda.web.resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import ck.panda.domain.entity.ThemeSetting;
 import ck.panda.service.ThemeSettingService;
 
+/** Home controller for theme setting */
 @RestController
 @RequestMapping("/home")
 public class HomeController {
